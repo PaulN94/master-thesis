c6ab6f6959e2d032e071a52f37ff7d73771ff09f
„Paul <paulnessensohn@web.de>
2023-09-22 14:44:02 +0200

    Vor dem Verändern des Basis Knapsack Modells

---
+++ script3_transform_with_LLM.py
@@ -0,0 +1,45 @@
+import os
+import openai
+import json
+
+# Warning: Do not expose your API key like this in production code.
+# Use environment variables or secret management services.
+api_key = "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"
+openai.api_key = api_key
+
+# System message
+system_message = "Modify the following Python optimization model according to the users question. Return only the modified executable code and nothing else"
+
+# Load the original JSON file
+input_json_filename = "JSON3_q_and_a_reformulation_knapsack_transformation.json"
+with open(input_json_filename, "r") as f:
+    data = json.load(f)
+
+# Read the content of knapsack_model.py
+with open("knapsack_model.py", "r") as f:
+    knapsack_model_content = f.read()
+
+# Iterate over each question and send it to OpenAI API
+for i, variation in enumerate(data['variations']):
+    question = variation['question_reformulation']
+
+    # Create the conversation
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": question},
+            {"role": "user", "content": knapsack_model_content}
+        ]
+    )
+
+    # Extract the transformed model
+    transformed_model = response['choices'][0]['message']['content']
+
+    # Add the transformed model to the question entry in the in-memory JSON object
+    data['variations'][i]['knapsack_model_transformed'] = transformed_model
+
+# Save the modified object to a new JSON file
+output_json_filename = "JSON4_q_and_a_llm_response_knapsack_transformation.json"
+with open(output_json_filename, "w") as f:
+    json.dump(data, f, indent=4)
---
+++ script4_solve_transformed_models.py
@@ -0,0 +1,31 @@
+import json
+
+def run_code_and_get_output(code):
+    try:
+        # Define the execution environment, if necessary
+        exec_globals = {}
+
+        # Execute the code
+        exec(code, exec_globals)
+
+        # Extract solver output from the global environment
+        solver_output = exec_globals.get('selected_items', 'No result')
+
+        return str(solver_output)
+
+    except Exception as e:
+        return f'Error: {e}'
+
+# Read input JSON file
+with open('JSON4_q_and_a_llm_response_knapsack_transformation.json', 'r') as f:
+    data = json.load(f)
+
+# Run Python code for each variation
+for variation in data['variations']:
+    knapsack_code = variation['knapsack_model_transformed']
+    solver_output = run_code_and_get_output(knapsack_code)
+    variation['llm_optimum'] = solver_output
+
+# Write new JSON file with solver outputs
+with open('JSON5_q_and_a_solved_transformed_models_knapsack_transformation.json', 'w') as f:
+    json.dump(data, f, indent=4)
